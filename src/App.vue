619ccf61447d44ab29e8f3c8b5d8e1ab91fb3915
Siracle <branson2015@gmail.com>
2021-05-25 14:03:55 -0400

    added basic GridStack

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <hello-world msg='Hello Vue'/>
-  <hello-moveable/>
+  <main-page/>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import HelloMoveable from './components/HelloMoveable.vue'
+//import HelloWorld from './components/HelloWorld.vue';
+//import HelloMoveable from './components/HelloMoveable.vue';
+
+import mainPage from './pages/main/main.vue';
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
---
+++ src/pages/main/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <UIBar />
+</template>
+
+<script>
+import UIBar from '../components/UIBar.vue';
+
+export default {
+  name: 'mainPage',
+  components: {
+    UIBar,
+  },
+  data: () => ({
+    
+  }),
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
