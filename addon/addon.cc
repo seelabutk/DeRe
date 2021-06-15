28f25c138425f79e02a4b958f34e0bdf8780a49c
James Hammer <branson2015@gmail.com>
2021-06-15 12:37:51 -0400

    screenshots now working

--- addon/addon.cc
+++ addon/addon.cc
@@ -64,6 +64,32 @@ Napi::Value getClientRect(const Napi::CallbackInfo &info){
   return obj;
 }
 
+Napi::Value getWindowRect(const Napi::CallbackInfo &info){
+  Napi::Env env = info.Env();
+
+  if(info.Length() != 1) {
+    Napi::TypeError::New(env, "Wrong number of arguments");
+    return env.Null();
+  }
+
+  if(!info[0].IsNumber()){
+    Napi::TypeError::New(env, "argument must be integer").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  RECT rect;
+  HWND hwnd = (HWND)info[0].As<Napi::Number>().Int64Value();
+  if(!GetWindowRect(hwnd, &rect))
+    return env.Null();
+
+  Napi::Object obj = Napi::Object::New(env);
+  obj.Set(Napi::String::New(env, "left"  ), Napi::Number::New(env, rect.left  ));
+  obj.Set(Napi::String::New(env, "top"   ), Napi::Number::New(env, rect.top   ));
+  obj.Set(Napi::String::New(env, "right" ), Napi::Number::New(env, rect.right ));
+  obj.Set(Napi::String::New(env, "bottom"), Napi::Number::New(env, rect.bottom));
+  return obj;
+}
+
 //https://stackoverflow.com/questions/26233848/c-read-pixels-with-getdibits
 //https://docs.microsoft.com/en-us/windows/win32/gdi/capturing-an-image
 //https://social.msdn.microsoft.com/Forums/vstudio/en-US/d08a599a-e1cc-4219-b76b-5bd0b4ea58d1/win32-how-use-getdibits-and-setdibits?forum=vclanguage
@@ -74,6 +100,7 @@ Napi::Value printWindow(const Napi::CallbackInfo &info){
   HDC hdcWindow = NULL;
   HDC hdc = NULL;
   HBITMAP hbmp = NULL;
+  BITMAP bmp;
   int width, height;
 
   if(info.Length() != 1) {
@@ -88,7 +115,7 @@ Napi::Value printWindow(const Napi::CallbackInfo &info){
   
   hwnd = (HWND)info[0].As<Napi::Number>().Int64Value();
   
-  GetClientRect(hwnd, &rect);
+  GetWindowRect(hwnd, &rect);
   width = rect.right - rect.left;
   height = rect.bottom - rect.top;
   
@@ -101,26 +128,37 @@ Napi::Value printWindow(const Napi::CallbackInfo &info){
   }
     
   HGDIOBJ hOld = SelectObject(hdc, hbmp);
-  BitBlt(hdc, rect.left, rect.top, width, height, hdcWindow, 0, 0, SRCCOPY|CAPTUREBLT);
+  BitBlt(hdc, -rect.left, -rect.top, width + rect.left, height + rect.top, hdcWindow, 0, 0, SRCCOPY|CAPTUREBLT);
   SelectObject(hdc, hOld);
   DeleteDC(hdc);
   
-  BITMAPINFO bmInfo = {0};
-  bmInfo.bmiHeader.biSize = sizeof(bmInfo.bmiHeader);
-  if(!GetDIBits(hdcWindow, hbmp, 0, 0, NULL, &bmInfo, DIB_RGB_COLORS)){
+  GetObject(hbmp, sizeof(BITMAP), &bmp);
+  BITMAPINFOHEADER   bi;
+
+  bi.biSize = sizeof(BITMAPINFOHEADER);
+  bi.biWidth = bmp.bmWidth;
+  bi.biHeight = -bmp.bmHeight;
+  bi.biPlanes = 1;
+  bi.biBitCount = 32;
+  bi.biCompression = BI_RGB;
+  bi.biSizeImage = 0;
+  bi.biXPelsPerMeter = 0;
+  bi.biYPelsPerMeter = 0;
+  bi.biClrUsed = 0;
+  bi.biClrImportant = 0;
+
+  size_t bitmapSize = ((bmp.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmp.bmHeight;
+  BYTE *pixels = new BYTE[bitmapSize];
+
+  if(!GetDIBits(hdcWindow, hbmp, 0, bi.biHeight, pixels, (BITMAPINFO*)&bi, DIB_RGB_COLORS)){
     Napi::TypeError::New(env, "failed to getDIBits").ThrowAsJavaScriptException();
     return env.Null();
   }
-
-  bmInfo.bmiHeader.biCompression = BI_RGB;
-  bmInfo.bmiHeader.biHeight = -bmInfo.bmiHeader.biHeight;
-  bmInfo.bmiHeader.biBitCount = 32;
-  size_t bitmapSize = bmInfo.bmiHeader.biSizeImage;
-  BYTE *pixels = new BYTE[bitmapSize];
-
-  if(!GetDIBits(hdcWindow, hbmp, 0, bmInfo.bmiHeader.biHeight, pixels, &bmInfo, DIB_RGB_COLORS)){
-    Napi::TypeError::New(env, "failed to getDIBits").ThrowAsJavaScriptException();
-    return env.Null();
+  
+  for(int i = 0; i < bitmapSize; i+=4){
+    BYTE tmp = pixels[i+0];
+    pixels[i+0] = pixels[i+2];
+    pixels[i+2] = tmp;
   }
   
   Napi::ArrayBuffer buffer = Napi::ArrayBuffer::New(env, pixels, bitmapSize,
@@ -150,6 +188,9 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
 
   exports.Set(Napi::String::New(env, "GetClientRect"),
     Napi::Function::New(env, getClientRect));
+
+  exports.Set(Napi::String::New(env, "GetWindowRect"),
+    Napi::Function::New(env, getWindowRect));
 
   exports.Set(Napi::String::New(env, "PrintWindow"),
     Napi::Function::New(env, printWindow));
--- addon/test/index.js
+++ addon/test/index.js
@@ -9,12 +9,14 @@ async function main(){
     await sleep(2000);
     let hwnd = addon.GetForegroundWindow();
     let text = addon.GetWindowText(hwnd);
-    let rect = addon.GetClientRect(hwnd);
+    let crect = addon.GetClientRect(hwnd);
+    let wrect = addon.GetWindowRect(hwnd);
     let screenshot = addon.PrintWindow(hwnd);
 
     console.log("Window hwnd: ", hwnd);
     console.log("Window Title: ", text);
-    console.log("Window rect: ", rect);
+    console.log("Client rect: ", crect);
+    console.log("Window rect: ", wrect);
     console.log("print window", screenshot);
 
 
