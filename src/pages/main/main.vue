03d22ca12781f9ec3c1df2417ee612de9a991ede
npatel79 <55924007+napatel79@users.noreply.github.com>
2022-01-04 13:28:50 -0600

    added COVID-19 Affects on School Finances

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -17,7 +17,7 @@ export default {
   },
   data: function(){
     const baseURL = 'https://raw.githubusercontent.com/branson2015/DeRe_Apps/main';
-    const apps = ['tableau', /* 'nytimes', 'median_household_income_2018', 'median_household_income_2019' */];
+    const apps = ['tableau', /* 'nytimes', 'median_household_income_2018', 'median_household_income_2019','COVID-19 Affects on School Finances' */];
     return {
       directories: apps.map(app => `${baseURL}/${app}/`),
     };
